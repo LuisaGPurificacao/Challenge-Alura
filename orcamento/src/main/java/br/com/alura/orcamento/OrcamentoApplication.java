e79484637a5a177574cff120e8ae68d7ee89dd26
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2022-12-21 14:08:34 -0300

    first commit - projeto inicial

---
+++ orcamento/src/main/java/br/com/alura/orcamento/OrcamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.orcamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoApplication.class, args);
+	}
+
+}
