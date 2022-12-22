86e175ffb888c44e0010575cfd19fbb41fd467f7
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2022-12-22 11:48:07 -0300

    Tratador de erros / exceptions adicionado

---
+++ orcamento/src/main/java/br/com/alura/orcamento/infra/exception/TratadorDeErros.java
@@ -0,0 +1,23 @@
+package br.com.alura.orcamento.infra.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException e) {
+        var erros = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
+}
